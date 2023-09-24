400c77b46b21e508706d209dbc13c2087b9b1fd8
MdZakirHossainBhuiyan <zakirhossaintuhin@gmail.com>
2023-09-24 14:17:55 +0600

    problem 1 is done

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,11 +1,46 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
+    const [inputValue, setInputValue] = useState({ name: '', status: '' });
+    const [dataList, setDataList] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+
+    useEffect(() => {
+        setFilteredData(dataList);
+    }, [dataList]);
 
     const handleClick = (val) =>{
         setShow(val);
+    }
+
+    useEffect(() => {
+        if (show === 'all') {
+            setFilteredData(dataList);
+        } else if (show === 'active') {
+            const filterData = dataList.filter((data) => data.status === 'active');
+            setFilteredData(filterData);
+        } else if (show === 'completed') {
+            const filterData = dataList.filter((data) => data.status === 'completed');
+            setFilteredData(filterData);
+        }
+    }, [show, dataList]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputValue((prevInputValue) => ({
+            ...prevInputValue,
+            [name]: value,
+        }));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        setDataList((prevDataList) => [...prevDataList, inputValue]);
+
+        setInputValue({ name: '', status: '' });
     }
 
     return (
@@ -14,12 +49,12 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form onSubmit={handleSubmit} className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input onChange={handleChange} name='name' type="text" className="form-control" placeholder="Name"/>
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input onChange={handleChange} name='status' type="text" className="form-control" placeholder="Status"/>
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -29,25 +64,32 @@ const Problem1 = () => {
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
                         </li>
                         <li className="nav-item">
                             <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
                         </li>
                     </ul>
                     <div className="tab-content"></div>
                     <table className="table table-striped ">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {
+                                filteredData?.map((data, index) => (
+                                    <tr key={index}>
+                                        <td scope="col">{data?.name}</td>
+                                        <td scope="col">{data?.status}</td>
+                                    </tr>
+                                ))
+                            }
                         </tbody>
                     </table>
                 </div>
